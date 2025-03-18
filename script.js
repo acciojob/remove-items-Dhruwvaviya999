e1715760727979a6e77192f6450b5ac471445b2a
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-18 23:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function handleClick() {
+	let colorSelected = document.getElementById("colorSelect").value;
+	let select =document.getElementById("colorSelect");
+	
+	let options = document.getElementsByTagName("option");
+
+	for(let i=0; i<options.length; i++){
+		if(options[i].innerText === colorSelected){
+			select.removeChild(options[i]);
+		};
+	};
+};
